5a6131101c92058cfb8e6dfb6c08964aa6849758
sunzhonghui <sunzhonghui@idmiss.com>
2026-08-20 23:03:26 +0800

    feat(config): include Cloudflare and local reverse proxy CIDRs in default trusted proxies

--- internal/config/config.go
+++ internal/config/config.go
@@ -125,8 +125,19 @@ func LoadConfig() *AppConfig {
 		BuildTime: BuildTime,
 	}
 
-	// Parse Trusted Proxy CIDRs
+	// Parse Trusted Proxy CIDRs (including Cloudflare and local reverse proxies by default)
+	defaultTrusted := "127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,::1/128,fc00::/7," +
+		"173.245.48.0/20,103.21.244.0/22,103.22.200.0/22,103.31.4.0/22,141.101.64.0/18,108.162.192.0/18," +
+		"190.93.240.0/20,188.114.96.0/20,197.234.240.0/22,198.41.128.0/17,162.158.0.0/15,104.16.0.0/13," +
+		"104.24.0.0/14,172.64.0.0/13,131.0.72.0/22,2400:cb00::/32,2606:4700::/32,2803:f800::/32,2405:b500::/32," +
+		"2405:8100::/32,2a06:98c0::/29,2c0f:f248::/32"
+
 	trustedStr := getEnv("TRUSTED_PROXY_CIDRS", "")
+	if trustedStr == "" {
+		trustedStr = getEnv("TRUSTED_PROXIES", defaultTrusted)
+	} else {
+		trustedStr = defaultTrusted + "," + trustedStr
+	}
 	cfg.TrustedProxyCIDRs = parseCIDRList(trustedStr)
 
 	// Parse DNS Resolvers
